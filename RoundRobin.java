a6df3bff2fd572133c7d64f927ebb191ebf089cc
mths0 <mhnd9919@gmail.com>
2025-03-04 23:33:45 +0300

    RR initial

---
+++ RoundRobin.java
@@ -0,0 +1,16 @@
+import java.util.Queue;
+
+public class RoundRobin extends Scheduler{
+
+    public RoundRobin(Queue<Job> readyQueue) {
+        super(readyQueue);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void scheduler() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scheduler'");
+    }
+    
+}
