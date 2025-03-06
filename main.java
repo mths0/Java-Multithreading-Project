658d9be2e9875988cdc92f9fd3a8622ab78b9ea5
mths0 <mhnd9919@gmail.com>
2025-03-06 21:14:39 +0300

    Testing Main

--- main.java
+++ main.java
@@ -1,1 +1,59 @@
 //main class
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class main {
+
+public static void main(String[] args) {
+    //initialize 
+    
+    Queue<Job> jobQueue = new LinkedList<>();
+    MemoryManager memoryManager = new MemoryManager();
+
+    //start job thread
+    Jobloader jobloader = new Jobloader(jobQueue);
+    jobloader.start();
+
+    //user select Scheduling Algorithm
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Select Scheduling Algorithm: [FCFS, RR, Priority]");
+    String choice = scanner.nextLine();
+
+    //select based on user choice
+    Scheduler scheduler = null;
+    if(choice.equalsIgnoreCase("FCFS")){
+        scheduler = new FCFSScheduler(jobQueue);
+    }else if(choice.equalsIgnoreCase("RR")){
+        scheduler = new RoundRobin(jobQueue);
+    // TODO make Priority class
+   /*  }else if(choice.equalsIgnoreCase("Priority")){
+        scheduler = new Priority(jobQueue);
+    }*/
+    }else{
+        System.out.println("Invalid choice. FCFS Running as Default .");
+        scheduler = new FCFSScheduler(jobQueue);
+    }
+
+    //start system handler (if needed)
+    
+    //start Scheduler Thread
+    scheduler.start();
+
+
+
+    //wait for all threads
+    try {
+        jobloader.join();
+        scheduler.join();
+    } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    
+    System.out.println("All jobs have been executed.");
+}
+
+
+}
