c3241ead7c15bd3ee5d79d007b29dfe12a6cd75c
Mohannad <mhnd9919@gmail.com>
2025-04-05 09:40:29 +0300

    private int originalBurstTime added

--- Job.java
+++ Job.java
@@ -1,102 +1,65 @@
-import java.util.Comparator;
-
 public class Job {
     private int id;
     private int burstTime;
+    private int originalBurstTime; // added
     private int priority;
     private int memoryRequired;
     private String state;
     private int waitingTime;
     private int turnaroundTime;
-    private int ArrivalTime;
+    private int arrivalTime;
 
-
-    public Job(int id, int burstTime, int priority, int memoryRequired){
-        this.id=id;
-        this.burstTime=burstTime;
-        this.priority=priority;
-        this.memoryRequired=memoryRequired;
-        this.state="New";
-        this.waitingTime=0;
-        this.turnaroundTime=0;
-        this.ArrivalTime=0;
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Job(int id, int burstTime, int priority, int memoryRequired) {
         this.id = id;
-    }
-
-    public int getBurstTime() {
-        return burstTime;
-    }
-
-    public void setBurstTime(int burstTime) {
         this.burstTime = burstTime;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
+        this.originalBurstTime = burstTime; 
         this.priority = priority;
-    }
-
-    public int getMemoryRequired() {
-        return memoryRequired;
-    }
-
-    public void setMemoryRequired(int memoryRequired) {
         this.memoryRequired = memoryRequired;
+        this.state = "New";
+        this.waitingTime = 0;
+        this.turnaroundTime = 0;
+        this.arrivalTime = 0;
     }
 
-    public String getState() {
-        return state;
-    }
+    // Getters and setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setState(String state) {
-        this.state = state;
-    }
+    public int getBurstTime() { return burstTime; }
+    public void setBurstTime(int burstTime) { this.burstTime = burstTime; }
 
-    public int getWaitingTime() {
-        return waitingTime;
-    }
+    public int getOriginalBurstTime() { return originalBurstTime; }
 
-    public void setWaitingTime(int waitingTime) {
-        this.waitingTime = waitingTime;
-    }
+    public int getPriority() { return priority; }
+    public void setPriority(int priority) { this.priority = priority; }
 
-    public int getTurnaroundTime() {
-        return turnaroundTime;
-    }
+    public int getMemoryRequired() { return memoryRequired; }
+    public void setMemoryRequired(int memoryRequired) { this.memoryRequired = memoryRequired; }
 
-    public void setTurnaroundTime(int turnaroundTime) {
-        this.turnaroundTime = turnaroundTime;
-    }
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
 
-    public void UpdateState(String NewState){
-        this.state=NewState;
-    }
+    public int getWaitingTime() { return waitingTime; }
+    public void setWaitingTime(int waitingTime) { this.waitingTime = waitingTime; }
 
-    public int getArrivalTime() {
-        return ArrivalTime;
-    }
+    public int getTurnaroundTime() { return turnaroundTime; }
+    public void setTurnaroundTime(int turnaroundTime) { this.turnaroundTime = turnaroundTime; }
 
-    public void setArrivalTime(int arrivalTime) {
-        ArrivalTime = arrivalTime;
-    }
+    public int getArrivalTime() { return arrivalTime; }
+    public void setArrivalTime(int arrivalTime) { this.arrivalTime = arrivalTime; }
+
+    public void updateState(String newState) { this.state = newState; }
 
     @Override
-	public String toString() {
-		return "Job [id=" + id + ", burstTime=" + burstTime + ", priority=" + priority + ", memoryRequired="
-				+ memoryRequired + ", state=" + state + ", waitingTime=" + waitingTime + ", turnaroundTime="
-				+ turnaroundTime + ", arrivaltime=" + ArrivalTime + "]";
-	}
-
-	
+    public String toString() {
+        return "Job [id=" + id +
+                ", burstTime=" + burstTime +
+                ", originalBurstTime=" + originalBurstTime +
+                ", priority=" + priority +
+                ", memoryRequired=" + memoryRequired +
+                ", state=" + state +
+                ", waitingTime=" + waitingTime +
+                ", turnaroundTime=" + turnaroundTime +
+                ", arrivalTime=" + arrivalTime + "]";
+    }
 }
