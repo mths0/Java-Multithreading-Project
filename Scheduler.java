02038633c2cdddd6a9b4fdcc0a3d4978dfa00b11
Mohannad <mhnd9919@gmail.com>
2025-04-05 09:42:56 +0300

    -GC() modified

--- Scheduler.java
+++ Scheduler.java
@@ -1,59 +1,66 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public abstract class Scheduler extends Thread{
-    
+public abstract class Scheduler extends Thread {
+
     protected MemoryManager memoryManager;
     protected Queue<Job> readyQueue;
-    protected Queue<Job> WitingQueue;
+    protected Queue<Job> waitingQueue;
     protected Queue<Job> executedQueue;
-    protected PriorityQueue<Job> PriorityQueue;
-    protected Queue<Job> JopQueue;
-    
-    public Scheduler(Queue<Job> JobQueue , MemoryManager memoryManager){
-    	this.JopQueue=JobQueue;
-    	this.memoryManager = memoryManager;
-    	readyQueue= new LinkedList<>(); 
-    	WitingQueue=new LinkedList<>(); 
-    	executedQueue=new LinkedList<>();
-    	
-    	
+    protected Queue<Job> jobQueue;
+
+    public Scheduler(Queue<Job> jobQueue, MemoryManager memoryManager) {
+        this.jobQueue = jobQueue;
+        this.memoryManager = memoryManager;
+
+        this.readyQueue = new LinkedList<>();
+        this.waitingQueue = new LinkedList<>();
+        this.executedQueue = new LinkedList<>();
     }
-   
- //thread to fill ready queue 
+
+    // Thread to fill ready queue (based on memory availability)
     @Override
-	public void run() {
-    	  while(!JopQueue.isEmpty()) {
-       	   Job currentJob=JopQueue.poll();
-       	   if(memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
-       		  readyQueue.add(currentJob);
-       		
-       	   }
-       	   else {
-       		   WitingQueue.add(currentJob);
-       	   }
-          }
-	}
-
-
-
-	public abstract void scheduler();
-
-    public void executeJob(Job job){
-
-        System.out.println("Executing Job ID: " + job.getId());
-        try {
-            Thread.sleep(job.getBurstTime() * 10);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public void run() {
+        while (!jobQueue.isEmpty()) {
+            Job currentJob = jobQueue.poll();
+            if (memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
+                readyQueue.add(currentJob);
+            } else {
+                waitingQueue.add(currentJob);
+            }
         }
-        System.out.println("Finished Job ID: " + job.getId());
-    }
-    public void calculateWaitingTime(){}
 
-    public void calculateTurnaroundTime(){}
+        // Start actual scheduling after job preparation
+        scheduler();
+    }
+
+    public abstract void scheduler();
     public abstract void addRemaindJop(int currentTime);
 
+    public void executeJob(Job job) {
+        //System.out.println("Executing Job ID: " + job.getId());
+        try {
+            Thread.sleep(job.getBurstTime() * 10); // Simulate execution time
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("Finished Job ID: " + job.getId());
+    }
 
+    public void calculateWaitingTime() {}  // can be overridden by child
+    public void calculateTurnaroundTime() {}
+
+    public String GC() {
+        StringBuilder chart = new StringBuilder();
+        int currentTime = 0;
+    
+        for (Job job : executedQueue) {
+            chart.append("|").append(currentTime).append(" P").append(job.getId()).append(" ");
+            currentTime += job.getBurstTime();
+        }
+    
+        chart.append("|").append(currentTime); // Final time at the end
+        return chart.toString();
+    }
+    
 }
