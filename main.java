178997a64f356bedfa09eb9ade2033d571d26862
Mohannad <mhnd9919@gmail.com>
2025-04-05 09:40:58 +0300

    memoryManager added to parm

--- main.java
+++ main.java
@@ -29,17 +29,9 @@ public static void main(String[] args) {
         
         
     }else if(choice.equalsIgnoreCase("RR")){
-<<<<<<< HEAD
-        scheduler = new RoundRobin(jobQueue);
-  
-     }else if(choice.equalsIgnoreCase("Priority")){
-        scheduler = new Priority(jobQueue);
-    
-=======
         scheduler = new RoundRobin(jobQueue, memoryManager);
      }else if(choice.equalsIgnoreCase("Priority")){
         scheduler = new Priority(jobQueue, memoryManager);
->>>>>>> branch 'main' of https://github.com/mths0/Java-Multithreading-Project
     }else{
         System.out.println("Invalid choice. FCFS Running as Default .");
         scheduler = new FCFSScheduler(jobQueue, memoryManager);
