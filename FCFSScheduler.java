c7cab4c2e725d6223f0959a97dd814a2f52d7463
Mohannad <mhnd9919@gmail.com>
2025-04-05 09:45:08 +0300

    -

--- FCFSScheduler.java
+++ FCFSScheduler.java
@@ -1,55 +1,66 @@
 import java.util.Queue;
 
-public class FCFSScheduler extends Scheduler{
+public class FCFSScheduler extends Scheduler {
 
-   
+    public FCFSScheduler(Queue<Job> jobQueue, MemoryManager memoryManager) {
+        super(jobQueue, memoryManager);
+    }
 
-    public FCFSScheduler(Queue<Job> JopQueue, MemoryManager memoryManager) {
-		super(JopQueue, memoryManager);
-		
-	}
-
-	@Override
+    @Override
     public void scheduler() {
         int currentTime = 0;
 
-        while(! readyQueue.isEmpty()){
+        while (!readyQueue.isEmpty()) {
             Job currentJob = readyQueue.poll();
-           executeJob(currentJob);
-           
-            currentJob.setWaitingTime(currentTime-currentJob.getArrivalTime());
-            currentTime += currentJob.getBurstTime();
-            executedQueue.add(currentJob);
-           if( memoryManager.deallocateMemory(currentJob.getMemoryRequired())) {
-        	   addRemaindJop(currentTime);
-           }
 
+            // Simulate execution
+            executeJob(currentJob);
+
+            // Set times
+            currentJob.setWaitingTime(currentTime - currentJob.getArrivalTime());
+            currentTime += currentJob.getBurstTime();
             currentJob.setTurnaroundTime(currentJob.getWaitingTime() + currentJob.getBurstTime());
+
+            executedQueue.add(currentJob);
+
+            if (memoryManager.deallocateMemory(currentJob.getMemoryRequired())) {
+                addRemaindJop(currentTime);
+            }
         }
+
+        // Print Gantt Chart
+        System.out.println("\nGantt Chart:");
         System.out.println(GC());
+
+        // Print Results
+        System.out.println("\nResults:");
+        int totalWaiting = 0;
+        int totalTurnaround = 0;
+
+        for (Job job : executedQueue) {
+            // System.out.println("Job " + job.getId() +
+            //         " | Waiting Time: " + job.getWaitingTime() +
+            //         " | Turnaround Time: " + job.getTurnaroundTime());
+
+            totalWaiting += job.getWaitingTime();
+            totalTurnaround += job.getTurnaroundTime();
+        }
+
+        System.out.printf("Average Waiting Time: %.2f ms\n", (double) totalWaiting / executedQueue.size());
+        System.out.printf("Average Turnaround Time: %.2f ms\n", (double) totalTurnaround / executedQueue.size());
     }
 
-	public void addRemaindJop(int currentTime) {
-		 while(!WitingQueue.isEmpty()) {
-	       	   Job currentJob=WitingQueue.peek();
-	       	   if(memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
-	       		currentJob.setArrivalTime(currentTime);
-	       		  readyQueue.add(currentJob);
-	       		  WitingQueue.poll();
-	       	   }
-	       	   else break;
-	       	   
-	          }
-		
-	}
-	public String GC() {
-		String process="";
-		while (!executedQueue.isEmpty()) {
-			process+=executedQueue.poll().getId()+"|";
-		}
-		return process;
-	}
-
-
-    
+    @Override
+    public void addRemaindJop(int currentTime) {
+        while (!waitingQueue.isEmpty()) {
+            Job currentJob = waitingQueue.peek();
+            if (memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
+                currentJob.setArrivalTime(currentTime);
+                readyQueue.add(currentJob);
+                waitingQueue.poll();
+            } else {
+                break;
+            }
+        }
+    }
 }
