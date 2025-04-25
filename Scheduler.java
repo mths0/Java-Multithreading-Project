84074ec768797615487e149d0c524ce664cf42bb
yasser2200 <y2868258@gmail.com>
2025-04-25 12:51:40 +0300

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -7,52 +7,62 @@ public abstract class Scheduler extends Thread {
     protected Queue<Job> readyQueue;
     protected Queue<Job> executedQueue;
     protected Queue<Job> jobQueue;
-
-    public Scheduler(Queue<Job> jobQueue, MemoryManager memoryManager) {
+    private Start_load Start_load;
+    public Scheduler(Queue<Job> jobQueue, MemoryManager memoryManager,Start_load Start_load) {
         this.jobQueue = jobQueue;
         this.memoryManager = memoryManager;
         this.readyQueue = new LinkedList<>();
         this.executedQueue = new LinkedList<>();
+        this.Start_load=Start_load;
     }
 
-    // Thread to fill ready queue (based on memory availability)
+    // Thread to fill ready queue and implement (based on memory availability)
     @Override
     public void run() {
-        while (!jobQueue.isEmpty()) {
+    	while(!Start_load.Start_load);// waiting to jobloader be load
+    	try {
+			Thread.sleep(55);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+    	double S=System.currentTimeMillis();
+        while (!jobQueue.isEmpty()||!readyQueue.isEmpty()) {
             Job currentJob = jobQueue.peek();
-            if (memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
+            if (currentJob!=null&&memoryManager.allocateMemory(currentJob.getMemoryRequired())) {
+            	
                 readyQueue.add(currentJob);
                 currentJob.setState("Ready");
                 System.out.println(currentJob.getId() + " " + currentJob.getState()); //! remove line
                 jobQueue.poll();
             } else {
-                break;
+            	scheduler();
 
             }
         }
+        double f=System.currentTimeMillis();
+        System.out.println("total time taken "+(f-S)/1000);
 
-        // Start actual scheduling after job preparation
-        scheduler();
+      
+        
     }
 
-    public abstract void scheduler();
-    public abstract void addRemindJop(int currentTime);
-
+    
+   
     public void executeJob(Job job) {
         
         job.setState("Running");
        
         try {
             // Simulate execution time
-            Thread.sleep(job.getBurstTime() ); 
+            Thread.sleep(job.getBurstTime()); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         
     }
-
-    public void calculateWaitingTime() {}  // can be overridden by child
-    public void calculateTurnaroundTime() {}
+    public abstract void scheduler();
+  
 
     public String GC() {
         StringBuilder chart = new StringBuilder();
