990b6a8e391fb6cce96dfc8d4acc9d810aaef759
yasser2200 <y2868258@gmail.com>
2025-04-25 12:52:15 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,43 +3,44 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class main {
 
 public static void main(String[] args) {
     //initialize 
     
-    Queue<Job> jobQueue = new LinkedList<>();
+	Queue<Job> jobQueue = new LinkedBlockingQueue<Job>();
     MemoryManager memoryManager = new MemoryManager();
-
+    Start_load start_load=new Start_load();
+    start_load.Start_load=false;
     //start job thread
-    Jobloader jobloader = new Jobloader(jobQueue);
-    jobloader.start();
-    System.out.println(jobQueue.toString());
-    System.out.println();
+    Jobloader jobloader = new Jobloader(jobQueue,start_load);
+    
+    //System.out.println();
 
     //user select Scheduling Algorithm
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Select Scheduling Algorithm: [FCFS, RR, Priority]");
+    System.out.println("\n Select Scheduling Algorithm: [FCFS, RR, Priority]:");
     String choice = scanner.nextLine();
 
     //select based on user choice
     Scheduler scheduler = null;
     if(choice.equalsIgnoreCase("FCFS")){
-        scheduler = new FCFSScheduler(jobQueue, memoryManager);
+        scheduler = new FCFSScheduler(jobQueue, memoryManager,start_load);
         
         
     }else if(choice.equalsIgnoreCase("RR")){
-        scheduler = new RoundRobin(jobQueue, memoryManager);
+        scheduler = new RoundRobin(jobQueue, memoryManager,start_load);
      }else if(choice.equalsIgnoreCase("Priority")){
-        scheduler = new Priority(jobQueue, memoryManager);
+        scheduler = new Priority(jobQueue, memoryManager,start_load);
     }else{
         System.out.println("Invalid choice. FCFS Running as Default .");
-        scheduler = new FCFSScheduler(jobQueue, memoryManager);
+        scheduler = new FCFSScheduler(jobQueue, memoryManager,start_load);
     }
-
-    //start system handler (if needed)
     
+    //start system handler (if needed)
+    jobloader.start();
     //start Scheduler Thread
     scheduler.start();
     
